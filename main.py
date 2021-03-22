70e95202ee5014d5940188825f7a85c2c3a11a44
Dmitriy <ice988@mail.ru>
2021-03-22 16:52:26 +0500

    Add args и kwargs

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ import os
 
 def logger(log_path):
     def decorator(search_countries):
-        def new_search_countries(file_path, search):
+        def new_search_countries(*args, **kwargs):
             date_today = datetime.datetime.now().strftime('%d.%m.%Y %H:%M')
             start = time.time()
-            result = search_countries(file_path, search)
+            result = search_countries(*args, **kwargs)
             finish = time.time() - start
             with open(log_path, 'a', encoding='utf-8') as ouf:
                 ouf.write(f'Имя функции - {search_countries.__name__}' + '\n')
                 ouf.write(f'Дата и время вызова функции - {date_today}' + '\n')
-                ouf.write(f'Аргументы функции - {file_path}, {search}' + '\n')
+                ouf.write(f'Аргументы функции - {args}, {kwargs}' + '\n')
                 ouf.write(f'Результат - {result}' + '\n')
                 ouf.write(f'Время работы функции - {finish}' + '\n')
                 ouf.write(f'Путь к логам - {os.path.abspath(log_path)}')
@@ -40,7 +40,7 @@ def search_countries(file_path, search):
 
     return countrie_dict
 
-search_countries('countries.json', 'United')
+search_countries('countries.json', 'Islands')
 
 
 
